4577ba482c81aee0b831a5739c5aa313c9bdddb3
Mattia A. Fritz <mattia.fritz@unige.ch>
2019-04-23 14:57:08 +0200

    Changes to perso

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,42 +11,41 @@ var currentURL = window.location.href;
 
 // Automatically add an active class to the main links in the navigation
 if (document.querySelectorAll("#main-tecfa-nav-links")) {
+  //Get all the links in the main navigation
+  var mainLinks = document.querySelectorAll("#main-tecfa-nav-links ul li");
 
-    //Get all the links in the main navigation
-    var mainLinks = document.querySelectorAll("#main-tecfa-nav-links ul li");
+  //Only for the homepage (first element in the array with 0 index)
+  var homepageURL = mainLinks[0].querySelector("a").href;
+  if (currentURL == homepageURL || currentURL == homepageURL + "index.html") {
+    mainLinks[0].classList.add("active");
+  }
 
-    //Only for the homepage (first element in the array with 0 index)
-    var homepageURL = mainLinks[0].querySelector("a").href;
-    if (currentURL == homepageURL || currentURL == homepageURL + "index.html") {
-        mainLinks[0].classList.add("active");
+  //For all the other links (starting from 1 in the loop then)
+  for (let i = 1; i < mainLinks.length; i++) {
+    let listLink = mainLinks[i].querySelector("a").href;
+    console.log(listLink);
+    if (currentURL.includes(listLink)) {
+      mainLinks[i].classList.add("active");
     }
-
-    //For all the other links (starting from 1 in the loop then)
-    for (let i = 1; i < mainLinks.length; i++) {
-        let listLink = mainLinks[i].querySelector("a").href;
-        console.log(listLink);
-        if (currentURL.includes(listLink)) {
-            mainLinks[i].classList.add("active");
-        }
-    }
+  }
 }
 
 /**
  * Breadcrumbs for perso pages
  */
 
-if (currentURL.includes("/perso/")) {
-    var pageLang = document.querySelector("html").lang;
-    breadcrumbsElement = document.querySelector("#breadcrumbs");
+// if (currentURL.includes("/perso/")) {
+//     var pageLang = document.querySelector("html").lang;
+//     breadcrumbsElement = document.querySelector("#breadcrumbs");
 
-    //Highlight the second element (index = 1) of the main nav
-    mainLinks[1].classList.add("active");
+//     //Highlight the second element (index = 1) of the main nav
+//     mainLinks[1].classList.add("active");
 
-    if (pageLang == "fr") {
-        let breadcrumbs = '<a href="/fr/">TECFA</a> &raquo; <a href="/fr/membres">Membres</a> &raquo; Page personelle</a>';
-        breadcrumbsElement.innerHTML = breadcrumbs;
-    } else {
-        let breadcrumbs = '<a href="/en/">TECFA</a> &raquo; <a href="/fr/people">People</a> &raquo; Personal page</a>';
-        breadcrumbsElement.innerHTML = breadcrumbs;
-    }
-}
+//     if (pageLang == "fr") {
+//         let breadcrumbs = '<a href="/fr/">TECFA</a> &raquo; <a href="/fr/membres">Membres</a> &raquo; Page personelle</a>';
+//         breadcrumbsElement.innerHTML = breadcrumbs;
+//     } else {
+//         let breadcrumbs = '<a href="/en/">TECFA</a> &raquo; <a href="/fr/people">People</a> &raquo; Personal page</a>';
+//         breadcrumbsElement.innerHTML = breadcrumbs;
+//     }
+// }
